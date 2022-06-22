ac54df5eb9309956593f617ae7d011c07d52bafd
manishagarwal99 <agarwal29manish@gmail.com>
2022-06-22 11:49:56 +0530

    a

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.use(express.static(__dirname + '/dist/wallet-angular-app'));
+app.get('/*', function(req,res)
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/wallet-angular-app'));
+{res.sendFile(path.join(__dirname+'/dist/wallet-angular-app/index.html'));});
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/wallet-angular-app/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
